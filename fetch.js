698bbb537e25c43d24f7521ec3ea3732e8915f0c
Cristhian Caycedo <cacaycedo@minenergia.gov.co>
2021-11-04 19:36:46 -0500

    fetch de otra manera

--- fetch.js
+++ fetch.js
@@ -1,5 +1,24 @@
 fetch('https://api.github.com/users/jdvpl/repos')
     .then(response => response.json())
-    .then(lol=> console.log(lol))
+    .then(lol => console.log(lol))
     .catch(e => console.log(e))
-    
+
+
+// ajax
+XMLHttpRequest
+
+// arquitectura soap
+/* <xml>
+</xml> */
+
+// arquitectura Rest
+
+// {name: "aklsdjflkasdf"}
+
+// endpoint https://jsonplaceholder.typicode.com/users/5
+fetch('https://jsonplaceholder.typicode.com/users/5')
+    .then((response) => {
+        return response.json()
+    }).then((data) => {
+        document.getElementById("info").innerHTML = data.name;
+    });
--- map.js
+++ map.js
@@ -1,10 +1,27 @@
-const datos=[
-    {nombre:"Juan Daniel", edad:15},
-    {nombre:"Juan David", edad:26},
-    {nombre:"Juan kakarot", edad:418},
-    {nombre:"Juan Saitama", edad:163},
-    {nombre:"Juan Luis", edad:19},
-    {nombre:"Juan Vergeta", edad:193},
+const datos = [{
+        nombre: "Juan Daniel",
+        edad: 15
+    },
+    {
+        nombre: "Juan David",
+        edad: 26
+    },
+    {
+        nombre: "Juan kakarot",
+        edad: 418
+    },
+    {
+        nombre: "Juan Saitama",
+        edad: 163
+    },
+    {
+        nombre: "Juan Luis",
+        edad: 19
+    },
+    {
+        nombre: "Juan Vergeta",
+        edad: 193
+    },
 ]
 
 // // con forEach
@@ -25,14 +42,14 @@ const datos=[
 
 
 
-const obtenerRepos=async()=>{
-    try{
+const obtenerRepos = async () => {
+    try {
         const response = await fetch('https://api.github.com/users/jdvpl/repos')
-        const datps= await response.json()
+        const datps = await response.json()
         datps.map(element => {
-            console.log(`id: ${element.id}, Nombre: ${element.name},URL: ${element.html_url}`)
+            document.write(`id: ${element.id}, Nombre: ${element.name},URL: ${element.html_url}`)
         })
-    }catch(err){
+    } catch (err) {
         console.log(err)
     }
 }
